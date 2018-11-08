4512e19608b5081e0ae54c2ab01511323058e9e1
unknown <jbrancale918@gmail.com>
2018-11-08 01:05:28 -0500

    worked on serverside netcode. Barbaric

--- OnlineSnake/src/online_snake/Main.java
+++ OnlineSnake/src/online_snake/Main.java
@@ -2,6 +2,9 @@ package online_snake;
 
 import java.awt.Point;
 import java.util.Random;
+import java.io.*;
+import java.net.*;
+import onlinetestcode.TransferPackage;
 
 public class Main {
 
@@ -19,24 +22,72 @@ public class Main {
 		Point applePoint = new Point(15,10);
 		game.put(applePoint, BoardObject.APPLE);
 		//Serversocket
+		ServerSocket serversocket = null;
+		Socket player1soc = null;
+		Socket player2soc = null;
+		try { 
+	         serverSocket = new ServerSocket(10007); 
+	        } 
+	    catch (IOException e) 
+	        { 
+	         System.err.println("Error on port: 10007."); 
+	         System.exit(1); 
+	        }
+		
 		//wait for socket 1
+		try { 
+	         System.out.println ("Waiting for Player 1");
+	         player1soc = serverSocket.accept(); 
+	        } 
+	    catch (IOException e) 
+	        { 
+	         System.err.println("Accept 1 failed."); 
+	         System.exit(1); 
+	        }
+		
 		//wait for socket 2
+		try { 
+	         System.out.println ("Waiting for Player 2");
+	         player2soc = serverSocket.accept(); 
+	        } 
+	    catch (IOException e) 
+	        { 
+	         System.err.println("Accept 2 failed."); 
+	         System.exit(1); 
+	        }
+		//initialize i/o streams for sockets
+		   ObjectOutputStream p1out = new ObjectOutputStream(
+                   clientSocket.getOutputStream()); 
+		   ObjectInputStream p1in = new ObjectInputStream( 
+                   clientSocket.getInputStream()); 
+		   ObjectOutputStream p2out = new ObjectOutputStream(
+                   clientSocket.getOutputStream()); 
+		   ObjectInputStream p2in = new ObjectInputStream( 
+                   clientSocket.getInputStream()); 
+		//initialize package for data transfer to client
+		   TransferPackage p1Package = null;
+		   TransferPackage p2Package = null;
+		
 		//initialize 2 input variables
-		String strDir = "";
-		Direction direction = Direction.enumFromValue(strDir);
-		int intPlayer = -1;
-		Player player = Player.enumFromValue(intPlayer);
+		String strDir1 = "";
+		String strDir2 = "";
+		Direction direction1 = null;
+		Direction direction2 = null; 
+		Player player1 = Player.enumFromValue(1);
+		Player player2 = Player.enumFromValue(2);
 		Player winner = null;
 
-		while(winner == null) {
+		while(winner == null) {				
 			boolean hasEaten = false;
 			
-			// get player and move of player
-			if (game.isLegalMove(player, direction)) {
+			strDir1 = (String)p1in.readObject();
+			direction1 = Direction.enumFromValue(strDir1);
+			// for player 1
+			if (game.isLegalMove(player1, direction)) {
 				
-				if (game.isEatMove(player, direction)) {
+				if (game.isEatMove(player1, direction)) {
 					hasEaten = true;
-					game.eat(player, direction);
+					game.eat(player1, direction);
 					winner = game.getWinner();
 					if (winner == null) { // continue the game
 						game.delete(applePoint);
@@ -46,20 +97,62 @@ public class Main {
 						// maybe print something to board
 					}
 				} else {
-					game.move(player, direction);					
+					game.move(player1, direction);					
 				}
 			} else {
-				winner = Player.getRival(player);
+				winner = Player.getRival(player1);
 			}
 			// hasEaten variable knows if eat occurs
-			Point playerHead = game.getPlayerSnake(player).getHead();
+			Point playerHead1 = game.getPlayerSnake(player1).getHead();
 			// socket returns playerHead and hasEaten to GUI
-			// socket receives input
-			strDir = "";
-			direction = Direction.enumFromValue(strDir);
-			intPlayer = -1;
-			player = Player.enumFromValue(intPlayer);
 			
+			
+			// socket receives input
+			strDir2 = (String)p2in.readObject();
+			direction2 = Direction.enumFromValue(strDir2);
+			
+			
+			
+			//for player 2
+			if (game.isLegalMove(player2, direction)) {
+				
+					if (game.isEatMove(player2, direction)) {
+						hasEaten = true;
+						game.eat(player, direction);
+						winner = game.getWinner();
+						if (winner == null) { // continue the game
+								game.delete(applePoint);
+								applePoint.setLocation(generateRandomPoint());
+								game.put(applePoint, BoardObject.APPLE);
+						} else { // max points reached
+							// maybe print something to board
+							}
+						} else {
+							game.move(player2, direction);					
+						}
+					} else {
+						winner = Player.getRival(player2);
+					}
+					// hasEaten variable knows if eat occurs
+					Point playerHead2 = game.getPlayerSnake(player2).getHead();
+					// socket returns playerHead and hasEaten to GUI
+					if(winner!=null){
+						if(winner == player1) {
+							p1Package = new TransferPackage(true);
+							p2Package = new TransferPackage(false);
+						}
+						else {
+							p1Package = new TransferPackage(false);
+							p2Package = new TransferPackage(true);
+						}
+					}
+					else {
+						p1Package = new TransferPackage(playerHead1,playerHead2);
+						p2Package = new TransferPackage(playerHead2,playerHead1);
+					}
+					
+					p1out.writeObject(p1Package);
+					p2out.writeObject(p2Package);
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
